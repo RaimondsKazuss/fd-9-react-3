5f6b689435e19342029b3d9315f70e864e0b330f
Raimonds Kazuss <rkazush@gmail.com>
2022-06-21 19:51:29 +0300

    sideBar layout and close button

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { colors, devices, zIndexes } from "../../theme/theme";
+import {
+  colors,
+  devices,
+  fontSizes,
+  margins,
+  paddings,
+  zIndexes,
+} from "../../theme/theme";
 
-const NavWrapper = styled.div`
+import xIcon from "../../assets/icons/icon-x.svg";
+import { useContext } from "react";
+import SideBarContext from "../../context/SideBarContext";
+
+const NavBackDrop = styled.div`
   width: 100%;
-  height: calc(100vh - 4rem);
   position: absolute;
-  right: 0;
+  left: 0;
   top: 4rem;
+  background: rgba(0, 0, 0, 0.2);
+`;
+
+const NavContent = styled.div`
+  width: 80%;
+  max-width: 18rem;
+  height: calc(100vh - 4rem);
   background: ${colors.bg};
   z-index: ${zIndexes.sideBar};
   box-shadow: -3px 3px 5px -5px ${colors.shadow};
@@ -21,19 +38,53 @@ const NavItem = styled(Link)`
   width: 100%;
   height: 4rem;
   background: ${colors.lightBlue};
+  display: block;
+  margin-bottom: ${margins.md};
+  padding-left: ${paddings.sm};
+  text-decoration: none;
+  color: ${colors.darkBlue};
+  line-height: 4rem;
+  box-sizing: border-box;
+`;
+
+const AuthLink = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 4rem;
+  color: ${colors.darkBlue};
+  line-height: 4rem;
+  padding-left: ${paddings.sm};
+  font-size: ${fontSizes.md};
+  box-sizing: border-box;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: ${margins.xs};
+  width: 2rem;
+  height: 2rem;
+  background: url(${xIcon}) center/contain no-repeat;
 `;
 
 const SideBar: React.FC = () => {
+  const { setIsSideBarOpen } = useContext(SideBarContext);
+
   return (
-    <NavWrapper>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-      <NavItem to="">CATEGORY NAME</NavItem>
-    </NavWrapper>
+    <NavBackDrop>
+      <CloseButton onClick={() => setIsSideBarOpen(false)} />
+      <NavContent>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <NavItem to="">CATEGORY NAME</NavItem>
+        <AuthLink>Login /Register </AuthLink>
+      </NavContent>
+    </NavBackDrop>
   );
 };
 
