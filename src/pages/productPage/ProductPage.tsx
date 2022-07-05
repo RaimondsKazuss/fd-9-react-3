5665ecfecf86c66b412248b9367f990d96335ad1
Raimonds Kazuss <rkazush@gmail.com>
2022-07-05 20:42:19 +0300

    product page responsive layout

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -2,21 +2,36 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { productInterface } from "../../interfaces";
-import { colors, paddings } from "../../theme/theme";
+import { colors, devices, margins, paddings } from "../../theme/theme";
 
 const ProductWrapper = styled.div`
   width: 100%;
   padding-top: ${paddings.md};
+  color: ${colors.darkBlue};
+  line-height: 1.5rem;
 `;
 
 const ProductImage = styled.div<{ bg: string }>`
   width: 100%;
   height: 20rem;
   background: url(${(props) => props && props.bg}) center/cover no-repeat;
+
+  ${devices.mobile} {
+    display: inline-block;
+    width: calc(50% - 0.75rem);
+    max-width: 20rem;
+    margin-right: ${margins.md};
+  }
 `;
 
 const ProductInfo = styled.div`
   width: 100%;
+
+  ${devices.mobile} {
+    display: inline-block;
+    width: calc(50% - 0.75rem);
+    max-width: 20rem;
+  }
 `;
 
 const AddToCart = styled.button`
@@ -32,6 +47,7 @@ const Price = styled.p`
 
 const Description = styled.div`
   width: 100%;
+  margin-top: ${margins.md};
 `;
 
 const ProductPage: React.FC = () => {
@@ -51,10 +67,13 @@ const ProductPage: React.FC = () => {
           <ProductImage bg={productData.images[0]} />
           <ProductInfo>
             <p>{productData.title}</p>
-            <Price>{productData.price}</Price>
+            <Price>${productData.price}</Price>
             <AddToCart>Add to cart</AddToCart>
           </ProductInfo>
-          <Description>{productData.description}</Description>
+          <Description>
+            {productData.description} {productData.description}{" "}
+            {productData.description} {productData.description}{" "}
+          </Description>
         </>
       ) : (
         <div>Loading...</div>
