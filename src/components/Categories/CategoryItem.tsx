4541de218979abd959a7c5743363ee070b506015
Raimonds Kazuss <rkazush@gmail.com>
2022-07-18 18:33:10 +0300

    experimental commit

--- src/components/Categories/CategoryItem.tsx
+++ src/components/Categories/CategoryItem.tsx
@@ -6,12 +6,6 @@ import { colors, margins } from "../../theme/theme";
 const Item = styled(Link)`
   width: calc(50% - 0.75rem);
   height: 6rem;
-  background: ${colors.lightBlue};
-  color: ${colors.darkBlue};
-  text-align: center;
-  line-height: 6rem;
-  margin-bottom: ${margins.md};
-  text-decoration: none;
 `;
 
 const CategoryItem: React.FC<{ category: string }> = ({ category }) => {
